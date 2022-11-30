82280e3999db12bede52ef0e3078a1820bd732ec
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-11-30 23:37:34 +0100

    add break

--- src/Object.cpp
+++ src/Object.cpp
@@ -10,7 +10,7 @@ Object::Object(const glm::vec2& pos, const glm::vec2& size):
 void Object::Draw(SDL_Renderer* renderer) const{
 	SDL_SetRenderDrawColor(renderer, 255, 255, 255, 0);
 	SDL_Rect dummy{
-		.x=static_cast<int>(position.x),
+			.x=static_cast<int>(position.x),
 			.y=static_cast<int>(position.y),
 			.w=static_cast<int>(size.x),
 			.h=static_cast<int>(size.y) 
@@ -26,6 +26,7 @@ std::ostream& operator<<(std::ostream& out, const glm::vec2& vec){
 	out<<"x: "<<vec.x<<", y: "<<vec.y;
 	return out;
 }
+
 static std::string CreateFormattedMessage(const char* prefix, const glm::vec2& vec){
 	std::stringstream ss;
 	ss<<prefix<<'{'<<vec<<"}\n";

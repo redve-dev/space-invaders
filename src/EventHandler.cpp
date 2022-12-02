5bdb01c0c491a1c378aea737a150faaa786caab6
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-12-02 19:51:05 +0100

    add basic event handler

---
+++ src/EventHandler.cpp
@@ -0,0 +1,36 @@
+#include "EventHandler.hpp"
+#include <iostream>
+#include <exception>
+
+EventHandler::EventHandler(){
+	buttons_state.resize(static_cast<size_t>(BUTTON::LENGTH), false);
+}
+
+static void UpdatePressedKeys(SDL_Keycode key, std::vector<bool>& buttons_state, bool state){
+	typedef EventHandler::BUTTON BUTTON;
+	switch(key){
+		case SDLK_a:
+			buttons_state.at(static_cast<size_t>(BUTTON::A)) = state;
+			break;
+		case SDLK_d:
+			buttons_state.at(static_cast<size_t>(BUTTON::D)) = state;
+			break;
+		case SDLK_SPACE:
+			buttons_state.at(static_cast<size_t>(BUTTON::SPACE)) = state;
+			break;
+	}
+}
+
+void EventHandler::UpdateKeys(const SDL_Event &e){
+	auto key = e.key.keysym.sym;
+	if(e.type == SDL_KEYDOWN){
+		UpdatePressedKeys(key, buttons_state, true);
+	}
+	if(e.type == SDL_KEYUP){
+		UpdatePressedKeys(key, buttons_state, false);
+	}
+}
+
+bool EventHandler::IsButtonPressed(BUTTON b){
+	return buttons_state.at(static_cast<size_t>(b));
+}
---
+++ src/EventHandler.hpp
@@ -0,0 +1,19 @@
+#include <SDL2/SDL.h>
+#include <vector>
+#include "Logger.hpp"
+
+class EventHandler{
+	std::vector<bool> buttons_state;
+	public:
+	enum class BUTTON{
+		A,
+		D,
+		SPACE,
+
+		LENGTH
+	};
+	EventHandler();
+	void UpdateKeys(const SDL_Event& e);
+	bool IsButtonPressed(BUTTON b);
+	friend class Logger;
+};

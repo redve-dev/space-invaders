e77722f2667f7bb753767d316cfb96466c5a1b52
Beniamin <106469071+redve-dev@users.noreply.github.com>
2022-12-02 17:57:51 +0100

    add constructor

--- src/Enemy.cpp
+++ src/Enemy.cpp
@@ -1,7 +1,13 @@
 #include "Enemy.hpp"
 
+Enemy::Enemy(const glm::i16vec2& pos):
+Entity(glm::vec2(0, 0)){
+	SetPosition(pos);
+
+}
+
 void Enemy::ReactToBullet(const glm::vec2& bullet_coordinates){
-	if (Hitbox.IsPointInside(bullet_coordinates)){
+	if (hitbox.IsPointInside(bullet_coordinates)){
 		IsAlive = false;
 	}
 }
--- src/Enemy.hpp
+++ src/Enemy.hpp
@@ -2,5 +2,6 @@
 
 class Enemy: public Entity{
 	public:
+	Enemy(const glm::i16vec2& pos);
 	void ReactToBullet(const glm::vec2& bullet_coordinates);
 };
